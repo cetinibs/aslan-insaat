40e04f5ea46df21508bad77e9dec120ad3305b55
ÇETİN KAYA <cetinkaya.pc@gmail.com>
2026-01-04 17:54:15 +0300

    fix: Varsayılan tema light (açık) mod olarak ayarlandı

--- components/providers.tsx
+++ components/providers.tsx
@@ -2,11 +2,19 @@
 
 import { ReactNode } from "react"
 import { LanguageProvider } from "@/lib/i18n"
+import { ThemeProvider } from "@/components/theme-provider"
 
 export function Providers({ children }: { children: ReactNode }) {
     return (
-        <LanguageProvider>
-            {children}
-        </LanguageProvider>
+        <ThemeProvider
+            attribute="class"
+            defaultTheme="light"
+            enableSystem={false}
+            disableTransitionOnChange
+        >
+            <LanguageProvider>
+                {children}
+            </LanguageProvider>
+        </ThemeProvider>
     )
 }
